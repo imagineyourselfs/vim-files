4a7ea2d20e4fe91835cac25149702d835cf76ed1
i00nsu <i00nsu@riseup.net>
2023-04-28 03:35:50 +0100

    Make it Right for a better Ride

---
+++ pushit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# IF you are using another domain to push your code... 
+
+pkill -x ssh-agent 
+sleep 1
+echo "ssh-agents killed"
+eval $(ssh-agent)
+sleep 1 
+echo "ssh-agent inicialized"
+ssh-add ~/.ssh/github_rsa
+sleep 1
+echo "ssh-key-id added"
+git add . 
+echo "all changes added to commit state"
+git commit -m "Make it Right for a better Ride"
+git push git@github.com:imagineyourselfs/vim-files
+echo "Push completed"
+sleep 3
+clear
